c7a91117d07a30b9111af960843c5d47be1f2ae2
Austin Kabiru <austin.kabiru@andela.com>
2017-09-13 16:08:39 +0300

    eureka

--- app/javascript/quotes/components/QuoteNavigation.jsx
+++ app/javascript/quotes/components/QuoteNavigation.jsx
@@ -1,1 +1,22 @@
 import React, { Component } from 'react'
+import { Link } from 'react-router-dom'
+
+const QuoteNavigation = props => {
+  const isPrevious = props.direction === 'previous'
+
+  return (
+    <Link
+      className={`link-${props.direction}`}
+      to={ `/?quote=${props.otherQuoteId}` }
+    >
+      <i
+        className={`fa fa-angle-${isPrevious ? 'left': 'right'}`}
+        aria-hidden='true'
+      >
+        <span />
+      </i>
+    </Link>
+  )
+}
+
+export default QuoteNavigation
