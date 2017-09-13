d3d061bd197812d3a892efe8c4bcccc082c98966
Austin Kabiru <austin.kabiru@andela.com>
2017-09-13 15:16:57 +0300

    split into more components

--- app/javascript/quotes/components/App.jsx
+++ app/javascript/quotes/components/App.jsx
@@ -1,16 +1,14 @@
 import React from 'react'
-import {
-  BrowserRouter as Router,
-  Route
-} from 'react-router-dom'
+import { BrowserRouter as Router, Route } from 'react-router-dom'
 import QuotesDisplay from './QuotesDisplay'
 
-const App = (props) => (
+const App = props => (
   <Router>
     <div>
       <Route
         path='/'
-        component={QuotesDisplay}
+        startingQuoteId={props.startingQuoteId}
+        render={routeProps => <QuotesDisplay {...props} {...routeProps} />}
       />
     </div>
   </Router>
--- app/javascript/quotes/index.js
+++ app/javascript/quotes/index.js
@@ -2,4 +2,9 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './components/App'
 
-ReactDOM.render(<App />, document.querySelector('#quotes'))
+const quotes = document.querySelector('#quotes')
+
+ReactDOM.render(
+  <App startingQuoteId={quotes.dataset.startingQuoteId} />,
+  quotes
+)
